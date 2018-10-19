2b745870ae1d62bbe2378f6536ef3c54674354ea
wangmingzhu <wangmingzhu@waiqin365.com>
2018-10-19 17:00:26 +0800

    seedsui redux middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 // Reducer
-import reducer from './modules/reducer.js'
+import reducer from './modules/reducer.js';
 // Middleware: thunk
 import thunk from 'redux-thunk';
 // Middleware: client
 import ApiAxios from 'seedsui-react/lib/ApiAxios';
-import clientMiddleware from './middleware/clientMiddleware';
+import requestMiddleware from 'seedsui-react/lib/ReduxRequestMiddleware';
 // Store
-const store = createStore(reducer, applyMiddleware(thunk, clientMiddleware(ApiAxios)));
+const store = createStore(reducer, applyMiddleware(thunk, requestMiddleware(ApiAxios)));
 
 export default store
