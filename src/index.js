0a6c0fe1253c1d3e76e3e77a36834d97a2326c1c
wangmingzhu <wangmingzhu@waiqin365.com>
2019-03-12 14:11:58 +0800

    src port

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ if (Device.platform === 'dinghuo' || Device.platform === 'waiqin') {
 // axios设置
 const env = process.env.NODE_ENV;
 if (env === 'development') {
-  // ApiAxios.setBaseURL(`http://172.31.3.96:8080/api`);
-  ApiAxios.setBaseURL(`http://localhost:8080/api`);
+  // ApiAxios.setBaseURL(`http://172.31.3.96:3000/api`);
+  ApiAxios.setBaseURL(`http://localhost:3000/api`);
 }
 
 // 处理401
